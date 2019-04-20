f2dec825dc22064c2c1ddba9912973a4813a7243
xlopezzzdom <koksharovsa@gmail.com>
2019-04-20 16:57:50 +0700

    Добавьте файлы проекта.

---
+++ TypeOfInjectors/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeOfInjectors
+{
+    public class Class1
+    {
+    }
+}
